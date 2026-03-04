4014c4dbc6129868f03b26555cd85227c9350200
spagnoll1andrea <andreaspagnolli40@gmail.com>
2026-03-04 01:18:47 +0100

    correzzione build

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <BrowserRouter basename="/novaflow-ascent">
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/portfolio" element={<Portfolio />} />
